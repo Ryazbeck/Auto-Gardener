152ef128b6c9074539030fc120330e49d5cc4622
russellyazbeck@gmail.com <russellyazbeck@gmail.com>
2020-07-12 15:54:18 -0500

    button, splash, and wifi setup work

---
+++ cleanup.sh
@@ -0,0 +1,7 @@
+sudo ndsctl stop
+sudo ifdown ap0
+sudo systemctl disable hostapd.service dnsmasq.service
+sudo systemctl stop hostapd.service dnsmasq.service
+sudo rm /etc/udev/rules.d/70-persistent-net.rules
+sudo udevadm trigger
+sudo systemctl restart networking.service
--- run_splash.py
+++ run_splash.py
@@ -1,11 +1,14 @@
 from gpiozero import Button
-from subprocess import check_call, run, PIPE
+from subprocess import check_call, run, PIPE, STDOUT, Popen, check_output
 from signal import pause
+from os import path, remove, system
+from time import sleep
+import re
 
-mac_address = run('cat /sys/class/net/wlan0/address', shell=True, stdin=PIPE, stdout=PIPE).stdout.read()
+mac_address = check_output('sudo cat /sys/class/net/wlan0/address', shell=True).decode('utf-8').replace('\n', '')
 
-ap_udev_rule = 'SUBSYSTEM=="ieee80211", ACTION=="add|change", ATTR\{macaddress\}=="b8:27:eb:32:24:84", KERNEL=="phy0", RUN+="/sbin/iw phy phy0 interface add ap0 type __ap", RUN+="/bin/ip link set ap0 address b8:27:eb:32:24:84"'
-ap_udev_rule_file = open("/etc/udev/rules.d/70-persistent-net.rules", "w")
+ap_udev_rule = f'SUBSYSTEM==\"ieee80211\", ACTION==\"add|change\", ATTR{{macaddress}}==\"{mac_address}\", KERNEL==\"phy0\", RUN+=\"/sbin/iw phy phy0 interface add ap0 type __ap\", RUN+=\"/bin/ip link set ap0 address {mac_address}\"'
+# ap_udev_rule_file = open("/etc/udev/rules.d/70-persistent-net.rules", "w")
 
 interfaces = '''
 source-directory /etc/network/interfaces.d
@@ -23,19 +26,112 @@ allow-hotplug wlan0
 iface wlan0 inet dhcp
         wpa-conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 '''
-interfaces_file = open("/etc/network/interfaces", "w")
+
+interfaces_clean = '''
+source-directory /etc/network/interfaces.d
+
+auto lo
+iface lo inet loopback
+
+allow-hotplug wlan0
+iface wlan0 inet dhcp
+        wpa-conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+'''
+
+def clean_up():
+  print("CLEANING UP...")
+  remove('/etc/udev/rules.d/70-persistent-net.rules')
+  interfaces_file = open('/etc/network/interfaces', 'w')
+  interfaces_file.write(interfaces_clean)
+  interfaces_file.close()
+  run('sudo systemctl disable hostapd.service dnsmasq.service', shell=True)
+  run('sudo systemctl stop hostapd.service dnsmasq.service', shell=True)
+  run('sudo ifdown ap0', shell=True)
+  run('sudo ndsctl stop', shell=True)
+  run('sudo udevadm trigger', shell=True)
+  run('sudo systemctl restart networking.service', shell=True)
 
 def run_splash():
-    print("Starting splash page...")
-    ap_udev_rule_file.write(ap_udev_rule)
-    ap_udev_rule_file.close()
+    print("Setting up AP Hotspot...")
 
-    interfaces_file.write(interfaces)
-    interfaces_file.close()
+    try:
+      print("Creating /etc/udev/rules.d/70-persistent-net.rules...")
+      udev_rules = open('/etc/udev/rules.d/70-persistent-net.rules', 'w+')
+      udev_rules.write(ap_udev_rule)
+      udev_rules.close()
+    except Exception as e:
+      print(f'Failed to create udev rule: {e}')
+      exit()
 
-    check_call(['sudo', 'ifup', 'ap0'])
+    try:
+      print("Updating /etc/network/interfaces...")
+      interfaces_file = open('/etc/network/interfaces', 'w')
+      interfaces_file.write(interfaces)
+      interfaces_file.close()
+    except Exception as e:
+      print(e)
+      remove('/etc/udev/rules.d/70-persistent-net.rules')
+      exit()
+
+    try:
+      print("Routing all traffic to gateway...")
+      check_call("sudo sed -e '/^#address/ s/^#//' -i /etc/dnsmasq.conf", shell=True)
+    except Exception as e:
+      print(f'Failed to update dnsmasq.conf: {e}')
+
+    print('Reloading udevadmin...')
+    check_call('sudo udevadm trigger', shell=True)
+    print('Restarting networking.service...')
+    check_call('sudo systemctl restart networking.service', shell=True)
+    
+    try:
+      print('Starting dnsmasq and hostapd...')
+      check_call('sudo systemctl enable hostapd.service dnsmasq.service', shell=True)
+      check_call('sudo systemctl start hostapd.service dnsmasq.service', shell=True)
+    except Exception as e:
+      if path.exists('/var/run/hostapd/ap0'):
+        print('Removing /var/run/hostapd/ap0...')
+        try:
+          check_call('sudo rm /var/run/hostapd/ap0', shell=True)
+        except Exception as e:
+          print(f'Unable to remove /var/run/hostapd/ap0: {e}')
+
+      sleep(3)
+      if system('sudo systemctl is-active --quiet hostapd') != 0:
+        print(f'hostapd failed: {e}')
+        clean_up()
+        exit()
+
+    print('Starting AP...')
+    check_call('sudo ip addr flush dev ap0', shell=True)
+    check_call('sudo ifup ap0', shell=True)
+    check_call('sudo ifdown ap0', shell=True)
+    check_call('sudo ifup ap0', shell=True)
+
+    try:
+      print('Starting nodogsplash...')
+      check_call('sudo nodogsplash &> /etc/nodogsplash/log.txt &', shell=True,
+        stdout=PIPE,
+        stderr=STDOUT)
+    except:
+      print('nodogsplash failed')
+
+
+
+    # iter = 0
+    # while iter < 30:
+    #   hostapd_log = check_call('sudo journalctl -u hostapd -n 15 --no-pager', shell=True).decode('utf-8')
+    #   for line in hostapd_log:
+    #     if 'invalid state' in line:
+    #       print('Invalid AP state detected. Restarting ap0...')
+    #       check_call('sudo ifdown ap0', shell=True)
+    #       check_call('sudo ifup ap0', shell=True)
+    #       iter = 30
+    #       break
+    #   sleep(2)
 
 button = Button(24, hold_time=3, pull_up=False)
 button.when_held = run_splash
 
+print("Button is ready...")
 pause()
